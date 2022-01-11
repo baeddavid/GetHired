a5444fd56e04cce20346223d64e345625d990008
David <baeddavid@gmail.com>
2022-01-11 16:36:07 -0600

    Initialized project

---
+++ src/main/java/com/service/gethired/GetHiredApplication.java
@@ -0,0 +1,13 @@
+package com.service.gethired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetHiredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetHiredApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/service/gethired/GetHiredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.gethired;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetHiredApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
