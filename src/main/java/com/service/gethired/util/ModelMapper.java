c1a722b92a36a0e01c0a16818aac26f4874ac406
David <baeddavid@gmail.com>
2022-01-24 00:30:43 -0600

    add utility

---
+++ src/main/java/com/service/gethired/util/ModelMapper.java
@@ -0,0 +1,30 @@
+package com.service.gethired.util;
+
+import com.service.gethired.model.Job;
+import com.service.gethired.model.User;
+import com.service.gethired.payload.UserSummary;
+import com.service.gethired.payload.JobResponse;
+
+import java.time.Instant;
+
+public class ModelMapper {
+    public static JobResponse mapJobToJobResponse(Job job, User creator) {
+        JobResponse jobResponse = new JobResponse();
+        jobResponse.setId(job.getId());
+        jobResponse.setJobListingName(job.getJobListingName());
+        jobResponse.setLink(job.getLink());
+        jobResponse.setCompany(job.getCompany());
+        jobResponse.setReferral(job.getReferral());
+        jobResponse.setStatus(job.getStatus());
+        jobResponse.setTags(job.getTags());
+        jobResponse.setCreationDateTime(job.getCreatedAt());
+        Instant now = Instant.now();
+        jobResponse.setExpired(job.getExpirationDate().isBefore(now));
+
+        System.out.println(creator.getEmail());
+        UserSummary creatorSummary = new UserSummary(creator.getId(), creator.getUsername(), creator.getName());
+        jobResponse.setCreatedBy(creatorSummary);
+
+        return jobResponse;
+    }
+}
