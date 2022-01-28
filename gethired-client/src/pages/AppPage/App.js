26b95c250d2147bacb9698bb4e8262e6fc8a9659
David <baeddavid@gmail.com>
2022-01-28 12:05:50 -0600

    add username state and signup pages

--- gethired-client/src/pages/AppPage/App.js
+++ gethired-client/src/pages/AppPage/App.js
@@ -2,25 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import { React, Component } from 'react';
 import { Routes , Route, Link } from "react-router-dom";
-import LoginHook from '../../components/Login.hook';
+import LoginHook from '../../components/LoginComponent/Login.hook';
+import Signup from "../../components/SignupComponent/signup.component";
 
 import AuthService from "../../services/auth.service";
-
+import UserService from "../../services/user.service";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentUser: undefined
+      currentUser: undefined,
+      username: undefined,
     }
   };
 
   componentDidMount() {
     const user = AuthService.getCurrentUser();
-
+    const userName = UserService.getUserInfo();
     if(user) {
       this.setState({
-        currentUser: user
+        currentUser: user,
+        username: UserService.getUsername().username
       });
     }
   }
@@ -29,10 +32,12 @@ class App extends Component {
     return(
       <div>
         <h1>Welcome to react router!</h1>
+        <button onClick={ () => console.log(this.state)}>Print State</button>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
-    <Route path="login" element={<LoginHook/>} />
+          <Route path="login" element={<LoginHook />} />
+          <Route path="signup" element={<Signup />} />
         </Routes>
       </div>
     )
