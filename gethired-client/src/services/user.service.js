6d9d1b4bcea58ba2a4d3c6ddd12a37dfeda8c668
David <baeddavid@gmail.com>
2022-01-28 12:08:21 -0600

    add user service calls

--- gethired-client/src/services/user.service.js
+++ gethired-client/src/services/user.service.js
@@ -1,3 +1,22 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
+const API_URL = "http://localhost:5000/api/user/";
+
+class UserService {
+    getUserInfo() {
+        axios.get(API_URL + "me", { headers: authHeader()})
+            .then(response => {
+                if(response.data.username) {
+                    localStorage.setItem("username", JSON.stringify(response.data));
+                }
+                return response.data;
+            });
+    }
+
+    getUsername() {
+        return JSON.parse(localStorage.getItem("username"));
+    }
+}
+
+export default new UserService();
