903a8933545f6655300598ef36d55ef9683cc424
David <baeddavid@gmail.com>
2022-01-28 21:47:20 -0600

    add POST newJob and GET user jobs

--- gethired-client/src/services/user.service.js
+++ gethired-client/src/services/user.service.js
@@ -2,8 +2,27 @@ import axios from 'axios';
 import authHeader from './auth-header';
 
 const API_URL = "http://localhost:5000/api/user/";
+const API_URL2 = "http://localhost:5000/api/users/";
+const API_URL3 = "http://localhost:5000/api/";
 
 class UserService {
+    createNewJob(jobListingName, link, company, referral, status, tags, jobLength) {
+        return axios
+            .post(API_URL3 + "jobs", {
+                jobListingName,
+                link,
+                company,
+                referral,
+                status,
+                tags,
+                jobLength
+            }, { headers: authHeader() });
+    }
+
+    getUserJobs(username) {
+       return axios.get(API_URL2 + username + "/jobs", { headers: authHeader() });
+    }
+
     getUserInfo() {
         axios.get(API_URL + "me", { headers: authHeader()})
             .then(response => {
