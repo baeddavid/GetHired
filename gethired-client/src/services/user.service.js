d59dbf0dbfe3e4de3c2936339b11f37f089210c4
David <baeddavid@gmail.com>
2022-01-29 23:36:34 -0600

    add get job by id, delete job and updatejob

--- gethired-client/src/services/user.service.js
+++ gethired-client/src/services/user.service.js
@@ -1,14 +1,12 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = "http://localhost:5000/api/user/";
-const API_URL2 = "http://localhost:5000/api/users/";
-const API_URL3 = "http://localhost:5000/api/";
+const API_URL = "http://localhost:5000/api/";
 
 class UserService {
     createNewJob(jobListingName, link, company, referral, status, tags, jobLength) {
         return axios
-            .post(API_URL3 + "jobs", {
+            .post(API_URL + "jobs", {
                 jobListingName,
                 link,
                 company,
@@ -19,12 +17,37 @@ class UserService {
             }, { headers: authHeader() });
     }
 
+    updateJob(jobListingName, link, company, referral, status, tags, jobLength, jobId) {
+        return axios
+            .put(API_URL + "jobs/" + jobId, {
+                jobListingName,
+                link,
+                company,
+                referral,
+                status,
+                tags,
+                jobLength
+            }, { headers: authHeader() });
+    }
+
+    deleteJob(jobId) {
+        return axios
+            .delete(API_URL + "jobs/" + jobId, { 
+                headers: authHeader()
+            });
+    }
+
+    getJobById(jobId) {
+        return axios
+            .get(API_URL + "jobs/" + jobId, { headers: authHeader() });
+    }
+
     getUserJobs(username) {
-       return axios.get(API_URL2 + username + "/jobs", { headers: authHeader() });
+       return axios.get(API_URL + "users/" + username + "/jobs", { headers: authHeader() });
     }
 
     getUserInfo() {
-        axios.get(API_URL + "me", { headers: authHeader()})
+        axios.get(API_URL + "user/" + "me", { headers: authHeader()})
             .then(response => {
                 if(response.data.username) {
                     localStorage.setItem("username", JSON.stringify(response.data));
