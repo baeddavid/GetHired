61dd85e71146071f9d2a62f76be46bbace6ac1bf
David <baeddavid@gmail.com>
2022-01-30 18:03:59 -0600

    add basic react placeholder for login page

--- gethired-client/src/components/LoginComponent/login.component.js
+++ gethired-client/src/components/LoginComponent/login.component.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Form from "react-validation/build/form";
 import Input from "react-validation/build/input";
 import CheckButton from "react-validation/build/button";
+import styles from "./LoginComponent.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/fontawesome-free-solid";
+import { faKey } from "@fortawesome/fontawesome-free-solid";
+
 
 import AuthService from "../../services/auth.service";
 
@@ -30,7 +35,7 @@ export default class Login extends Component {
             message: ""
         };
     }
-    
+
     onChangeUsername(e) {
         this.setState({
             username: e.target.value
@@ -83,70 +88,82 @@ export default class Login extends Component {
 
     render() {
         return (
-            <div className="col-md-12">
-                <div className="card card-container">
-                    <img
-                        src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-                        alt="profile-img"
-                        className="profile-img-card"
-                    />
-
-                    <Form
-                        onSubmit={this.handleLogin}
-                        ref={c => {
-                            this.form = c;
-                        }}
-                    >
-                        <div className="form-group">
-                            <label htmlFor="username">Username</label>
-                            <Input
-                                type="text"
-                                className="form-control"
-                                name="username"
-                                value={this.state.username}
-                                onChange={this.onChangeUsername}
-                                validations={[required]}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="password">Password</label>
-                            <Input
-                                type="password"
-                                className="form-control"
-                                name="password"
-                                value={this.state.password}
-                                onChange={this.onChangePassword}
-                                validations={[required]}
-                            />
-                        </div>
-
-                        <div className="form-group">
-                            <button
-                                className="btn btn-primary btn-block"
-                                disabled={this.state.loading}
-                            >
-                                {this.state.loading && (
-                                    <span className="spinner-border spinner-border-sm"></span>
-                                )}
-                                <span>Login</span>
-                            </button>
-                        </div>
-
-                        {this.state.message && (
-                            <div className="form-group">
-                                <div className="alert alert-danger" role="alert">
-                                    {this.state.message}
-                                </div>
+            <div className={styles.background}>
+                <div className="container col-xs-1" align="center" id={styles.center}>
+                    <div className={styles.userCard}>
+                        <div>
+                            <div className="d-flex justify-content-center">
+                                <h2 className={styles.title} style={{ color: "white" }}>GetHired</h2>
                             </div>
-                        )}
-
-                        <CheckButton
-                            style={{ display: "none" }}
+                        </div>
+                        <Form
+                            onSubmit={this.handleLogin}
                             ref={c => {
-                                this.checkBtn = c;
+                                this.form = c;
                             }}
-                        />
-                    </Form>
+                        >
+                            <div className="input-group mb-3">
+                                <div className="input-group-append">
+                                    <span className={`input-group-text ${styles.icon}`}>
+                                        <FontAwesomeIcon icon={faUser} style={{ color: "white" }} />
+                                    </span>
+                                </div>
+                                <Input
+                                    type="text"
+                                    className="form-control"
+                                    name="username"
+                                    value={this.state.username}
+                                    onChange={this.onChangeUsername}
+                                    validations={[required]}
+                                    placeholder="Username or Email"
+                                />
+                            </div>
+                            <div className="input-group mb-2">
+                                <div className="input-group-append">
+                                    <span className={`input-group-text ${styles.icon}`}>
+                                        <FontAwesomeIcon icon={faKey} style={{ color: "white" }} />
+                                    </span>
+                                </div>
+                                <Input
+                                    type="password"
+                                    className="form-control"
+                                    name="password"
+                                    value={this.state.password}
+                                    onChange={this.onChangePassword}
+                                    validations={[required]}
+                                    placeholder="Password"
+                                />
+                            </div>
+                            <div className="d-flex justify-content-center mt-3 login_container">
+                                <button className="btn btn-success" id={styles.button}>Log in</button> <br />
+                            </div>
+                            {this.state.message && (
+                                <div className="form-group">
+                                    <div className="alert alert-danger" role="alert">
+                                        {this.state.message}
+                                    </div>
+                                </div>
+                            )}
+                            <CheckButton
+                                style={{ display: "none" }}
+                                ref={c => {
+                                    this.checkBtn = c;
+                                }}
+                            />
+                        </Form>
+                        <div className="mt-4">
+                            <div className="d-flex justify-content-center links">
+                                Don't have an account? 
+                            </div>
+                            <div className="d-flex justify-content-center links">
+                                <Link to="/signup">
+                                    <div className="ml-2" style={{ color: "white" }}>
+                                        Sign Up
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
